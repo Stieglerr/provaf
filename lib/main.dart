a5b37b825ca9aab1e3d004f38d021278733c0ecc
Stieglerr <stieglerstr@hotmail.com>
2024-10-08 21:31:07 -0300

    first_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'tela_inicial.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+
+Future main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+ runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
